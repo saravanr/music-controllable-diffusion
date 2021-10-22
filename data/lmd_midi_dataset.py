06ce48ab21df7ab9b44ad4a27576179e65b96c75
Saravana Rathinam <saravanr@icloud.com>
2021-10-22 08:27:04 -0700

    Update data loader

--- data/lmd_midi_dataset.py
+++ data/lmd_midi_dataset.py
@@ -3,9 +3,36 @@ import numpy as np
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 from utils.file_utils import get_files_in_path
-import magenta
+from utils.midi_utils import MidiConverter
+from torchvision import transforms
 
-class LMDMidiDataset(Dataset):
+
+def data_loader_collate_fn(batch):
+    """
+    Custom collate function to remove Nones without throwing exceptions
+    :param batch: The batch
+    :return: Collate Fun
+    """
+    batch = list(filter(lambda x: x is not None, batch))
+    return torch.utils.data.dataloader.default_collate(batch)
+
+
+class Reshape(object):
+    """
+    Reshapes the sample to be
+    """
+
+    def __init__(self, shape):
+        self._shape = shape
+
+    def __call__(self, sample):
+        if sample is None:
+            return None
+        sample = sample.reshape(self._shape)
+        return sample
+
+
+class MidiDataset(Dataset):
 
     def __init__(self, data_dir, transform=None):
         self.data_dir = data_dir
@@ -19,33 +46,36 @@ class LMDMidiDataset(Dataset):
         if torch.is_tensor(index):
             raise NotImplementedError(f"Torch indexes are not implemented")
         midi_file_name = self.data_files[index]
-        mid = MidiFile(midi_file_name)
 
-        # Midi types:
-        # 0 -> Single track
-        # 1 -> Synchronous
-        # 2 -> Asynchronous
-        midi_type = mid.type
-        print(mid)
-        sample = mid
-        magenta.scripts
-        if self.transform:
+        try:
+            sample = MidiConverter().load_file(midi_file_name).to_nd_array()
+        except Exception as e:
+            print(f"Unable to load {midi_file_name} possibly corrupt? - {e}")
+            sample = None
+
+        if sample is not None and self.transform:
             sample = self.transform(sample)
-
 
         return sample
 
 
 if __name__ == "__main__":
     import os
+    data_shape = (245, 286)
     _data_dir = os.path.expanduser("~/midi/")
-    _data_set = LMDMidiDataset(_data_dir)
+    _data_set = MidiDataset(_data_dir,
+                            transform=transforms.Compose([Reshape(data_shape)]))
 
     print(f"Data set length = {len(_data_set)}")
     print(f"Data set sample = {_data_set[10]}")
 
-    _dataloader = DataLoader(_data_set, batch_size=4, shuffle=True, num_workers=8)
+    _dataloader = DataLoader(_data_set,
+                             batch_size=4,
+                             shuffle=True,
+                             num_workers=15,
+                             collate_fn=data_loader_collate_fn,
+
+                             )
 
     for _batch, _sample in enumerate(_dataloader):
-        print(f"_batch={_batch}")
-
+        print(f"_batch={_batch} _sample_shape={_sample.shape}")
--- utils/midi_utils.py
+++ utils/midi_utils.py
@@ -26,12 +26,17 @@ def play_note_seq(note_sequence_object):
 
 class MidiConverter(object):
     """
-    Converts Midi files to Numpy arrays and vice-versa for training/eval purposes
+    Converts Midi files to Numpy arrays and vice-versa for training/eval purposes. Default max note size is 10010 (multiple
+    of 7)
+    since the percentiles of notes lengths are:
+    [p25, p50,   p75,   p95,    p99]
+    [4.0, 234.0, 863.0, 3263.0, 9636.359999999986]
     """
 
-    def __init__(self):
+    def __init__(self, max_notes=10010):
         self._note_seq = None
         self._nd_array = None
+        self._max_notes = max_notes
         pass
 
     def load_file(self, midi_file_path):
@@ -62,6 +67,9 @@ class MidiConverter(object):
             raise RuntimeError(f"Please load a midi file")
 
         note_sequence_ordered = list(self._note_seq.notes)
+        if len(note_sequence_ordered) > self._max_notes:
+            # Trim
+            note_sequence_ordered = note_sequence_ordered[0:self._max_notes]
 
         # We do not include other header information present in NoteSeq. This may be lossy
         # but as long as we have the notes and instruments, we should have some version of the
@@ -81,6 +89,9 @@ class MidiConverter(object):
 
         nd_array = np.stack([pitches, velocities, instruments, programs, start_times, end_times, delta_times], axis=1) \
             .astype(np.float32)
+        diff_rows = self._max_notes - nd_array.shape[0]
+        if diff_rows > 0:
+            nd_array = np.pad(nd_array, ((0, diff_rows), (0, 0)), constant_values=0)
         self._nd_array = nd_array
         return nd_array
 
@@ -108,6 +119,7 @@ class MidiConverter(object):
 
 
 if __name__ == "__main__":
-    nd_array = MidiConverter().load_file("/home/joy/midi/lmd_full/d/d9ef4f22e5bf77cae6bda79c50887267.mid").to_nd_array()
-    note_seq = MidiConverter().load_nd_array(nd_array).to_note_seq()
-    play_note_seq(note_seq)
+    _nd_array = MidiConverter().load_file(
+        "/home/joy/midi/lmd_full/d/d9ef4f22e5bf77cae6bda79c50887267.mid").to_nd_array()
+    _note_seq = MidiConverter().load_nd_array(_nd_array).to_note_seq()
+    play_note_seq(_note_seq)
