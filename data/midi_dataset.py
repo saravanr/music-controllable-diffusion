Fix KL Loss

data/midi_dataset.py
@@ -1,4 +1,7 @@
 import torch
+import numpy as np
+import os
+import shutil
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 
@@ -33,9 +36,11 @@ class Reshape(object):
 
 class MidiDataset(Dataset):
 
-    def __init__(self, data_dir, transform=None):
+    def __init__(self, data_dir, transform=None, junk_dir=os.path.expanduser("~/midi_junk")):
         self.data_dir = data_dir
         self.transform = transform
+        self.junk_dir = junk_dir
+        os.makedirs(junk_dir, exist_ok=True)
         self.data_files = get_files_in_path(data_dir, matching_pattern="*.mid")
 
     def __len__(self):
@@ -45,12 +50,17 @@ class MidiDataset(Dataset):
         if torch.is_tensor(index):
             raise NotImplementedError(f"Torch indexes are not implemented")
         midi_file_name = self.data_files[index]
-
+        numpy_file_name = f"{midi_file_name}.npy"
         try:
-            sample = MidiConverter().load_file(midi_file_name).to_nd_array()
-        except Exception as e:
-            print(f"Unable to load {midi_file_name} possibly corrupt? - {e}")
+            if os.path.exists(numpy_file_name):
+                sample = np.load(numpy_file_name)
+            else:
+                sample = MidiConverter().load_file(midi_file_name).to_nd_array()
+                np.save(numpy_file_name, sample)
+        except Exception as _:
             sample = None
+            base_name = os.path.basename(midi_file_name)
+            shutil.move(midi_file_name, os.path.join(self.junk_dir, base_name))
 
         if sample is not None and self.transform:
             sample = self.transform(sample)
@@ -71,7 +81,7 @@ if __name__ == "__main__":
     _dataloader = DataLoader(_data_set,
                              batch_size=4,
                              shuffle=True,
-                             num_workers=15,
+                             num_workers=8,
                              collate_fn=data_loader_collate_fn,
 
                              )

models/simple_vae/simple_vae.py
@@ -1,10 +1,12 @@
+import os.path
+
 import torch
 from pytorch_lightning.utilities.types import STEP_OUTPUT
-
+from data.midi_data_module import MidiDataModule
 from models.base.base_model import BaseModel
 from torch import nn
 from torch.nn import functional as func
-
+from pytorch_lightning import Trainer
 
 class Encoder(nn.Module):
     """
@@ -14,20 +16,20 @@ class Encoder(nn.Module):
     def __init__(self, z_dim, input_shape):
         super().__init__()
         self._z_dim = z_dim
-        flattened_input = input_shape[0] * input_shape[1]
+        flattened_input = (input_shape[0] * input_shape[1]) // 10
 
         self._net = nn.Sequential(
-            nn.Linear(input_shape[0] * input_shape[1], flattened_input / 2),
+            nn.Linear(input_shape[0] * input_shape[1], flattened_input // 2),
             nn.LeakyReLU(),
-            nn.Linear(flattened_input / 2, flattened_input / 2),
+            nn.Linear(flattened_input // 2, flattened_input // 2),
             nn.LeakyReLU(),
-            nn.Linear(flattened_input / 2, flattened_input / 4),
+            nn.Linear(flattened_input // 2, flattened_input // 4),
             nn.LeakyReLU(),
-            nn.Linear(flattened_input / 4, flattened_input / 8)
+            nn.Linear(flattened_input // 4, flattened_input // 8)
         )
 
-        self._fc_mean = nn.Linear(flattened_input/8, z_dim)
-        self._fc_var = nn.Linear(flattened_input/8, z_dim)
+        self._fc_mean = nn.Linear(flattened_input // 8, z_dim)
+        self._fc_var = nn.Linear(flattened_input // 8, z_dim)
 
     def forward(self, x):
         x = torch.flatten(x, start_dim=1)
@@ -45,19 +47,21 @@ class Decoder(nn.Module):
     def __init__(self, z_dim, output_shape):
         super().__init__()
         self._z_dim = z_dim
-        flattened_output = output_shape[0] * output_shape[1]
+        self._output_shape = output_shape
+        flattened_output = (output_shape[0] * output_shape[1]) // 10
         self._net = nn.Sequential(
-            nn.Linear(z_dim, flattened_output / 4),
+            nn.Linear(z_dim, flattened_output // 4),
             nn.LeakyReLU(),
-            nn.Linear(flattened_output / 4, flattened_output / 2),
+            nn.Linear(flattened_output // 4, flattened_output // 2),
             nn.LeakyReLU(),
-            nn.Linear(flattened_output / 2, flattened_output / 2),
+            nn.Linear(flattened_output // 2, flattened_output // 2),
             nn.LeakyReLU(),
-            nn.Linear(flattened_output / 2, flattened_output)
+            nn.Linear(flattened_output // 2, flattened_output * 10)
         )
 
     def forward(self, z):
         output = self._net(z)
+        output = output.reshape((-1, self._output_shape[0], self._output_shape[1]))
         return output
 
 
@@ -66,32 +70,56 @@ class SimpleVae(BaseModel):
         super().__init__()
         self._encoder = Encoder(z_dim, input_shape=input_shape)
         self._decoder = Decoder(z_dim, output_shape=input_shape)
+        self.z_prior_m = torch.nn.Parameter(torch.zeros(1), requires_grad=False)
+        self.z_prior_v = torch.nn.Parameter(torch.ones(1), requires_grad=False)
 
     @staticmethod
     def sample(mean, log_var):
-        std = torch.exp(log_var / 2)
-        p = torch.distributions.Normal(torch.zeros_like(mean), torch.ones_like(std))
+        # Set all small values to epsilon
+        std = func.softplus(log_var) + 1e-8
         q = torch.distributions.Normal(mean, std)
         z = q.rsample()
         return z
 
     @staticmethod
-    def _kl(mean, log_var):
-        kl_loss = torch.mean(-0.5 * torch.sum(1 + log_var - mean ** 2 - log_var.exp(), dim =1), dim=0)
+    def _kl_normal(qm, qv, pm, pv):
+        """
+        Computes the elem-wise KL divergence between two normal distributions KL(q || p) and
+        sum over the last dimension
+
+        Args:
+            qm: tensor: (batch, dim): q mean
+            qv: tensor: (batch, dim): q variance
+            pm: tensor: (batch, dim): p mean
+            pv: tensor: (batch, dim): p variance
+
+        Return:
+            kl: tensor: (batch,): kl between each sample
+        """
+        element_wise = 0.5 * (torch.log(pv) - torch.log(qv) + qv / pv + (qm - pm).pow(2) / pv - 1)
+        kl = element_wise.sum(-1)
+        return kl
+
+    def _kl(self, mean, var):
+        eps = 1e-8
+        kl_loss = -0.5 * torch.sum(1 + torch.log(var + eps) - torch.square(mean) - var, axis=-1)
+        kl_loss = torch.mean(kl_loss)
         return kl_loss
 
     def forward(self, x):
-        mean, log_var = self._encoder.forward(x)
-        z = SimpleVae.sample(mean, log_var)
+        mean, var = self._encoder.forward(x)
+        relu = nn.ReLU()
+        var = relu(var)
+        z = SimpleVae.sample(mean, var)
         x_hat = self._decoder(z)
-        return z, x_hat, mean, log_var
+        return z, x_hat, mean, var
 
     def step(self, batch, batch_idx):
         x = batch
         z, x_hat, pm, pv = self.forward(x)
 
         recon_loss = func.mse_loss(x_hat, x, reduction='mean')
-        kl = SimpleVae._kl(pm, pv)
+        kl = self._kl(pm, pv)
         loss = recon_loss + kl
         logs = {
             "recon_loss": recon_loss,
@@ -105,7 +133,24 @@ class SimpleVae(BaseModel):
         print(f"Training Reconstruction Loss={logs['recon_loss']} KL Loss={logs['kl_loss']} Total Loss={logs['loss']}")
         return loss
 
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self._lr)
 
+
+if __name__ == "__main__":
+    print(f"Training simple VAE")
+    # torch.autograd.set_detect_anomaly(True)
+    z_dim = 200
+    input_shape = (245, 286)
+    model = SimpleVae(z_dim=z_dim, input_shape=input_shape)
+    dm = MidiDataModule(
+        data_dir=os.path.expanduser("~/midi/"),
+        batch_size=40,
+    )
+    trainer = Trainer(auto_scale_batch_size="power",
+                      gpus=1)
+    trainer.fit(model, dm)
+    trainer.save_checkpoint("simplevae.chkpoint")
 
 
 