1ee09e7564a23cc46357c9094d4ac740c835d213
Saravana Rathinam <joy@elbo.ai>
2021-11-23 22:19:52 -0800

    Add score sde

--- models/score/sde.py
+++ models/score/sde.py
@@ -1,40 +1,26 @@
 import functools
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import numpy as np
+import torch.nn as nn
 
-
-class GaussianFourierProjection(nn.Module):
-    """Gaussian random features for encoding time steps."""
-
-    def __init__(self, embed_dim, scale=30.):
-        super().__init__()
-        # Randomly sample weights during initialization. These weights are fixed
-        # during optimization and are not trainable.
-        self.W = nn.Parameter(torch.randn(embed_dim // 2) * scale, requires_grad=False)
-
-    def forward(self, x):
-        x_proj = x[:, None] * self.W[None, :] * 2 * np.pi
-        return torch.cat([torch.sin(x_proj), torch.cos(x_proj)], dim=-1)
-
-
-class Dense(nn.Module):
-    """A fully connected layer that reshapes outputs to feature maps."""
-
-    def __init__(self, input_dim, output_dim):
-        super().__init__()
-        self.dense = nn.Linear(input_dim, output_dim)
-
-    def forward(self, x):
-        return self.dense(x)[..., None, None]
+from models.score.score_sde_samplers import ode_sampler
+from utils.cuda_utils import get_device
+from utils.model_utils import GaussianFourierProjection, Dense
+import torch
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from torchvision.datasets import MNIST
+import tqdm
+import os
+from torchvision.utils import make_grid
+import matplotlib.pyplot as plt
 
 
 class ScoreNet(nn.Module):
     """A time-dependent score-based model built upon U-Net architecture."""
 
-    def __init__(self, marginal_prob_std, channels=[32, 64, 128, 256], embed_dim=256):
+    def __init__(self, marginal_prob_std, channels=None, embed_dim=256):
         """Initialize a time-dependent score-based network.
 
         Args:
@@ -45,6 +31,8 @@ class ScoreNet(nn.Module):
         """
         super().__init__()
         # Gaussian random feature embedding layer for time
+        if channels is None:
+            channels = [32, 64, 128, 256]
         self.embed = nn.Sequential(GaussianFourierProjection(embed_dim=embed_dim),
                                    nn.Linear(embed_dim, embed_dim))
         # Encoding layers where the resolution decreases
@@ -123,9 +111,6 @@ class ScoreNet(nn.Module):
         return h
 
 
-device = 'cpu'  # @param ['cuda', 'cpu'] {'type':'string'}
-
-
 def marginal_prob_std(t, sigma):
     """Compute the mean and standard deviation of $p_{0t}(x(t) | x(0))$.
 
@@ -136,6 +121,7 @@ def marginal_prob_std(t, sigma):
     Returns:
       The standard deviation.
     """
+    device = get_device()
     t = torch.tensor(t, device=device)
     return torch.sqrt((sigma ** (2 * t) - 1.) / 2. / np.log(sigma))
 
@@ -150,15 +136,12 @@ def diffusion_coeff(t, sigma):
     Returns:
       The vector of diffusion coefficients.
     """
+    device = get_device()
     return torch.tensor(sigma ** t, device=device)
 
 
-sigma = 25.0  # @param {'type':'number'}
-marginal_prob_std_fn = functools.partial(marginal_prob_std, sigma=sigma)
-diffusion_coeff_fn = functools.partial(diffusion_coeff, sigma=sigma)
-
 def loss_fn(model, x, marginal_prob_std, eps=1e-5):
-  """The loss function for training score-based generative models.
+    """The loss function for training score-based generative models.
 
   Args:
     model: A PyTorch model instance that represents a
@@ -168,246 +151,78 @@ def loss_fn(model, x, marginal_prob_std, eps=1e-5):
       the perturbation kernel.
     eps: A tolerance value for numerical stability.
   """
-  random_t = torch.rand(x.shape[0], device=x.device) * (1. - eps) + eps
-  z = torch.randn_like(x)
-  std = marginal_prob_std(random_t)
-  perturbed_x = x + z * std[:, None, None, None]
-  score = model(perturbed_x, random_t)
-  loss = torch.mean(torch.sum((score * std[:, None, None, None] + z)**2, dim=(1,2,3)))
-  return loss
-
-#@title Training (double click to expand or collapse)
-
-import torch
-import functools
-from torch.optim import Adam
-from torch.utils.data import DataLoader
-import torchvision.transforms as transforms
-from torchvision.datasets import MNIST
-import tqdm
+    random_t = torch.rand(x.shape[0], device=x.device) * (1. - eps) + eps
+    z = torch.randn_like(x)
+    std = marginal_prob_std(random_t)
+    perturbed_x = x + z * std[:, None, None, None]
+    score = model(perturbed_x, random_t)
+    loss = torch.mean(torch.sum((score * std[:, None, None, None] + z) ** 2, dim=(1, 2, 3)))
+    return loss
 
 
-score_model = torch.nn.DataParallel(ScoreNet(marginal_prob_std=marginal_prob_std_fn))
-score_model = score_model.to(device)
+def process():
+    device = get_device()
+    score_model = torch.nn.DataParallel(ScoreNet(marginal_prob_std=marginal_prob_std_fn))
+    n_epochs = 50  # @param {'type':'integer'}
+    if os.path.exists('ckpt.pth'):
+        ckpt = torch.load('ckpt.pth', map_location=device)
+        score_model.load_state_dict(ckpt)
+        n_epochs = 2
 
-n_epochs =   50#@param {'type':'integer'}
-## size of a mini-batch
-batch_size =  32 #@param {'type':'integer'}
-## learning rate
-lr=1e-4 #@param {'type':'number'}
+    score_model = score_model.to(device)
+    ## size of a mini-batch
+    batch_size = 32  # @param {'type':'integer'}
+    ## learning rate
+    lr = 1e-4  # @param {'type':'number'}
 
-dataset = MNIST('.', train=True, transform=transforms.ToTensor(), download=True)
-data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    dataset = MNIST('.', train=True, transform=transforms.ToTensor(), download=True)
+    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=0)
 
-optimizer = Adam(score_model.parameters(), lr=lr)
-tqdm_epoch = tqdm.notebook.trange(n_epochs)
-for epoch in tqdm_epoch:
-  avg_loss = 0.
-  num_items = 0
-  for x, y in data_loader:
-    x = x.to(device)
-    loss = loss_fn(score_model, x, marginal_prob_std_fn)
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    avg_loss += loss.item() * x.shape[0]
-    num_items += x.shape[0]
-  # Print the averaged training loss so far.
-  tqdm_epoch.set_description('Average Loss: {:5f}'.format(avg_loss / num_items))
-  # Update the checkpoint after each epoch of training.
-  torch.save(score_model.state_dict(), 'ckpt.pth')
+    optimizer = Adam(score_model.parameters(), lr=lr)
 
-# @title Define the Euler-Maruyama sampler (double click to expand or collapse)
+    for epoch in tqdm.tqdm(range(0, n_epochs)):
+        avg_loss = 0.
+        num_items = 0
 
-## The number of sampling steps.
-num_steps = 500  # @param {'type':'integer'}
+        for x, y in data_loader:
+            x = x.to(device)
+            loss = loss_fn(score_model, x, marginal_prob_std_fn)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            loss = loss.item()
+            avg_loss += loss * x.shape[0]
+            num_items += x.shape[0]
+            print(f"Epoch = {epoch} Loss = {loss}")
+        # Print the averaged training loss so far.
+        print('Average Loss: {:5f}'.format(avg_loss / num_items))
+        # Update the checkpoint after each epoch of training.
+        torch.save(score_model.state_dict(), 'ckpt.pth')
 
+    sample_batch_size = 64  # @param {'type':'integer'}
+    sampler = ode_sampler  # @param ['Euler_Maruyama_sampler', 'pc_sampler', 'ode_sampler'] {'type': 'raw'}
 
-def Euler_Maruyama_sampler(score_model,
-                           marginal_prob_std,
-                           diffusion_coeff,
-                           batch_size=64,
-                           num_steps=num_steps,
-                           device='cuda',
-                           eps=1e-3):
-    """Generate samples from score-based models with the Euler-Maruyama solver.
+    ## Generate samples using the specified sampler.
+    samples = sampler(score_model,
+                      marginal_prob_std_fn,
+                      diffusion_coeff_fn,
+                      sample_batch_size,
+                      device=device)
 
-    Args:
-      score_model: A PyTorch model that represents the time-dependent score-based model.
-      marginal_prob_std: A function that gives the standard deviation of
-        the perturbation kernel.
-      diffusion_coeff: A function that gives the diffusion coefficient of the SDE.
-      batch_size: The number of samplers to generate by calling this function once.
-      num_steps: The number of sampling steps.
-        Equivalent to the number of discretized time steps.
-      device: 'cuda' for running on GPUs, and 'cpu' for running on CPUs.
-      eps: The smallest time step for numerical stability.
+    ## Sample visualization.
+    samples = samples.clamp(0.0, 1.0)
 
-    Returns:
-      Samples.
-    """
-    t = torch.ones(batch_size, device=device)
-    init_x = torch.randn(batch_size, 1, 28, 28, device=device) \
-             * marginal_prob_std(t)[:, None, None, None]
-    time_steps = torch.linspace(1., eps, num_steps, device=device)
-    step_size = time_steps[0] - time_steps[1]
-    x = init_x
-    with torch.no_grad():
-        for time_step in tqdm.notebook.tqdm(time_steps):
-            batch_time_step = torch.ones(batch_size, device=device) * time_step
-            g = diffusion_coeff(batch_time_step)
-            mean_x = x + (g ** 2)[:, None, None, None] * score_model(x, batch_time_step) * step_size
-            x = mean_x + torch.sqrt(step_size) * g[:, None, None, None] * torch.randn_like(x)
-            # Do not include any noise in the last sampling step.
-    return mean_x
+    sample_grid = make_grid(samples, nrow=int(np.sqrt(sample_batch_size)))
+
+    plt.figure(figsize=(6, 6))
+    plt.axis('off')
+    plt.imshow(sample_grid.permute(1, 2, 0).cpu(), vmin=0., vmax=1.)
+    plt.show()
 
 
-# @title Define the Predictor-Corrector sampler (double click to expand or collapse)
+if __name__ == "__main__":
+    sigma = 25.0  # @param {'type':'number'}
+    marginal_prob_std_fn = functools.partial(marginal_prob_std, sigma=sigma)
+    diffusion_coeff_fn = functools.partial(diffusion_coeff, sigma=sigma)
+    process()
 
-signal_to_noise_ratio = 0.16  # @param {'type':'number'}
-
-## The number of sampling steps.
-num_steps = 500  # @param {'type':'integer'}
-
-
-def pc_sampler(score_model,
-               marginal_prob_std,
-               diffusion_coeff,
-               batch_size=64,
-               num_steps=num_steps,
-               snr=signal_to_noise_ratio,
-               device='cuda',
-               eps=1e-3):
-    """Generate samples from score-based models with Predictor-Corrector method.
-
-    Args:
-      score_model: A PyTorch model that represents the time-dependent score-based model.
-      marginal_prob_std: A function that gives the standard deviation
-        of the perturbation kernel.
-      diffusion_coeff: A function that gives the diffusion coefficient
-        of the SDE.
-      batch_size: The number of samplers to generate by calling this function once.
-      num_steps: The number of sampling steps.
-        Equivalent to the number of discretized time steps.
-      device: 'cuda' for running on GPUs, and 'cpu' for running on CPUs.
-      eps: The smallest time step for numerical stability.
-
-    Returns:
-      Samples.
-    """
-    t = torch.ones(batch_size, device=device)
-    init_x = torch.randn(batch_size, 1, 28, 28, device=device) * marginal_prob_std(t)[:, None, None, None]
-    time_steps = np.linspace(1., eps, num_steps)
-    step_size = time_steps[0] - time_steps[1]
-    x = init_x
-    with torch.no_grad():
-        for time_step in tqdm.notebook.tqdm(time_steps):
-            batch_time_step = torch.ones(batch_size, device=device) * time_step
-            # Corrector step (Langevin MCMC)
-            grad = score_model(x, batch_time_step)
-            grad_norm = torch.norm(grad.reshape(grad.shape[0], -1), dim=-1).mean()
-            noise_norm = np.sqrt(np.prod(x.shape[1:]))
-            langevin_step_size = 2 * (snr * noise_norm / grad_norm) ** 2
-            x = x + langevin_step_size * grad + torch.sqrt(2 * langevin_step_size) * torch.randn_like(x)
-
-            # Predictor step (Euler-Maruyama)
-            g = diffusion_coeff(batch_time_step)
-            x_mean = x + (g ** 2)[:, None, None, None] * score_model(x, batch_time_step) * step_size
-            x = x_mean + torch.sqrt(g ** 2 * step_size)[:, None, None, None] * torch.randn_like(x)
-
-            # The last step does not include any noise
-        return x_mean
-
-
-# @title Define the ODE sampler (double click to expand or collapse)
-
-from scipy import integrate
-
-## The error tolerance for the black-box ODE solver
-error_tolerance = 1e-5  # @param {'type': 'number'}
-
-
-def ode_sampler(score_model,
-                marginal_prob_std,
-                diffusion_coeff,
-                batch_size=64,
-                atol=error_tolerance,
-                rtol=error_tolerance,
-                device='cuda',
-                z=None,
-                eps=1e-3):
-    """Generate samples from score-based models with black-box ODE solvers.
-
-    Args:
-      score_model: A PyTorch model that represents the time-dependent score-based model.
-      marginal_prob_std: A function that returns the standard deviation
-        of the perturbation kernel.
-      diffusion_coeff: A function that returns the diffusion coefficient of the SDE.
-      batch_size: The number of samplers to generate by calling this function once.
-      atol: Tolerance of absolute errors.
-      rtol: Tolerance of relative errors.
-      device: 'cuda' for running on GPUs, and 'cpu' for running on CPUs.
-      z: The latent code that governs the final sample. If None, we start from p_1;
-        otherwise, we start from the given z.
-      eps: The smallest time step for numerical stability.
-    """
-    t = torch.ones(batch_size, device=device)
-    # Create the latent code
-    if z is None:
-        init_x = torch.randn(batch_size, 1, 28, 28, device=device) \
-                 * marginal_prob_std(t)[:, None, None, None]
-    else:
-        init_x = z
-
-    shape = init_x.shape
-
-    def score_eval_wrapper(sample, time_steps):
-        """A wrapper of the score-based model for use by the ODE solver."""
-        sample = torch.tensor(sample, device=device, dtype=torch.float32).reshape(shape)
-        time_steps = torch.tensor(time_steps, device=device, dtype=torch.float32).reshape((sample.shape[0],))
-        with torch.no_grad():
-            score = score_model(sample, time_steps)
-        return score.cpu().numpy().reshape((-1,)).astype(np.float64)
-
-    def ode_func(t, x):
-        """The ODE function for use by the ODE solver."""
-        time_steps = np.ones((shape[0],)) * t
-        g = diffusion_coeff(torch.tensor(t)).cpu().numpy()
-        return -0.5 * (g ** 2) * score_eval_wrapper(x, time_steps)
-
-    # Run the black-box ODE solver.
-    res = integrate.solve_ivp(ode_func, (1., eps), init_x.reshape(-1).cpu().numpy(), rtol=rtol, atol=atol,
-                              method='RK45')
-    print(f"Number of function evaluations: {res.nfev}")
-    x = torch.tensor(res.y[:, -1], device=device).reshape(shape)
-
-    return x
-
-#@title Sampling (double click to expand or collapse)
-
-from torchvision.utils import make_grid
-
-## Load the pre-trained checkpoint from disk.
-device = 'cuda' #@param ['cuda', 'cpu'] {'type':'string'}
-ckpt = torch.load('ckpt.pth', map_location=device)
-score_model.load_state_dict(ckpt)
-
-sample_batch_size = 64 #@param {'type':'integer'}
-sampler = ode_sampler #@param ['Euler_Maruyama_sampler', 'pc_sampler', 'ode_sampler'] {'type': 'raw'}
-
-## Generate samples using the specified sampler.
-samples = sampler(score_model,
-                  marginal_prob_std_fn,
-                  diffusion_coeff_fn,
-                  sample_batch_size,
-                  device=device)
-
-## Sample visualization.
-samples = samples.clamp(0.0, 1.0)
-import matplotlib.pyplot as plt
-sample_grid = make_grid(samples, nrow=int(np.sqrt(sample_batch_size)))
-
-plt.figure(figsize=(6,6))
-plt.axis('off')
-plt.imshow(sample_grid.permute(1, 2, 0).cpu(), vmin=0., vmax=1.)
-plt.show()
