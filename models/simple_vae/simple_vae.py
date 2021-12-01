eb9b5ad3e192f340252a49d2dbc2d9e1c2779914
Saravana Rathinam <saravanr@icloud.com>
2021-11-30 16:06:12 -0800

    Add LSTM and poster template

--- models/simple_vae/simple_vae.py
+++ models/simple_vae/simple_vae.py
@@ -19,6 +19,29 @@ import wandb
 wandb.init(project="music-controllable-diffusion-midi", entity="saravanr")
 
 
+class ExtractLSTMOutput(nn.Module):
+    def __init__(self, extract_out=True):
+        super(ExtractLSTMOutput, self).__init__()
+        self._extract_out = extract_out
+
+    def forward(self, x):
+        out, hidden = x
+        if self._extract_out:
+            return out
+        else:
+            return hidden
+
+
+class Reshape1DTo2D(nn.Module):
+    def __init__(self, output_shape):
+        super(Reshape1DTo2D, self).__init__()
+        self._output_shape = output_shape
+
+    def forward(self, x):
+        x = x.view((-1, self._output_shape[0], self._output_shape[1]))
+        return x
+
+
 class Encoder(nn.Module):
     """
     VAE Encoder takes the input and maps it to latent representation Z
@@ -32,13 +55,20 @@ class Encoder(nn.Module):
         else:
             scale = 1
 
-        input_dim = (input_shape[0] * input_shape[1]) // scale
+        seq_len = input_shape[0]
+        seq_width = input_shape[1]
+        input_dim = (seq_len * seq_width) // scale
+
         self._net = nn.Sequential(
-            nn.Linear(input_shape[0] * input_shape[1], input_dim // 2),
+            nn.LSTM(input_size=seq_len, hidden_size=seq_len, num_layers=4, batch_first=True),
+            ExtractLSTMOutput(),
+            nn.Flatten(start_dim=1),
+            nn.Linear(input_dim, input_dim // 2),
             nn.ReLU(),
+            nn.Dropout(0.2),
             nn.Linear(input_dim // 2, input_dim // 4),
             nn.Tanh(),
-            nn.Dropout(0.5),
+            nn.Dropout(0.2),
             nn.Linear(input_dim // 4, input_dim // 4),
             nn.ReLU(),
             nn.Linear(input_dim // 4, input_dim // 8)
@@ -50,11 +80,14 @@ class Encoder(nn.Module):
         self._fc_log_var = nn.Sequential(
             nn.Linear(input_dim // 8, z_dim),
         )
+        self._device = get_device()
+        self._seq_len = seq_len
+        self._seq_width = seq_width
 
     def forward(self, x):
-        x = torch.flatten(x, start_dim=1)
-        h = self._net(x)
 
+        x = x.reshape((-1, self._seq_width, self._seq_len))
+        h = self._net(x)
         mean = self._fc_mean(h)
         log_var = self._fc_log_var(h)
         return mean, log_var
@@ -69,13 +102,15 @@ class Decoder(nn.Module):
         super(Decoder, self).__init__()
         self._z_dim = z_dim
         self._output_shape = output_shape
+        seq_len = output_shape[0]
+        seq_width = output_shape[1]
 
         if output_shape[0] < 100:
             scale = 1
         else:
             scale = 1
 
-        output_dim = (output_shape[0] * output_shape[1]) // scale
+        output_dim = (seq_len * seq_width) // scale
         self._net = nn.Sequential(
             nn.Linear(z_dim, output_dim // 4),
             nn.ReLU(),
@@ -83,14 +118,21 @@ class Decoder(nn.Module):
             nn.Tanh(),
             nn.Linear(output_dim // 2, output_dim // 4),
             nn.ReLU(),
+            nn.Dropout(0.2),
             nn.Linear(output_dim // 4, output_dim * scale),
+            Reshape1DTo2D((seq_width, seq_len)),
+            nn.LSTM(input_size=seq_len, hidden_size=seq_len, num_layers=4, batch_first=True),
+            ExtractLSTMOutput(),
             nn.Tanh()
         )
+        self._seq_len = seq_len
+        self._seq_width = seq_width
 
     def forward(self, z):
         output = self._net(z)
         # Clamp output in (0, 1) to prevent errors in BCE
         # output = torch.clamp(output, 1e-8, 1 - 1e-8)
+        output = output.reshape((-1, self._seq_len, self._seq_width))
         return output
 
     @property
@@ -115,8 +157,8 @@ class SimpleVae(BaseModel):
         return z, x_hat, mean, log_var
 
     def loss_function(self, x_hat, x, mu, q_log_var):
-        #recon_loss = func.binary_cross_entropy(x_hat, x.view(-1, 784), reduction='sum')
-        recon_loss = func.mse_loss(x_hat, x.view(-1, MAX_MIDI_ENCODING_ROWS*MIDI_ENCODING_WIDTH), reduction='sum')
+        # recon_loss = func.binary_cross_entropy(x_hat, x.view(-1, 784), reduction='sum')
+        recon_loss = func.mse_loss(x_hat, x, reduction='sum')
         kl = self._kl_simple(mu, q_log_var)
         loss = recon_loss + self.alpha * kl
         return loss
@@ -146,7 +188,7 @@ class SimpleVae(BaseModel):
             with torch.no_grad():
                 device = get_device()
                 if False:
-                    #TODO
+                    # TODO
                     rand_z = torch.randn(16, self._decoder.z_dim).to(device)
                     rand_z.to(device)
                     output = self._decoder(rand_z)
@@ -184,8 +226,8 @@ if __name__ == "__main__":
         )
     else:
         model = SimpleVae(
-            alpha=1,
-            z_dim=80,
+            alpha=1000,
+            z_dim=256,
             input_shape=(MAX_MIDI_ENCODING_ROWS, MIDI_ENCODING_WIDTH),
             use_mnist_dms=False,
             sample_output_step=10,
@@ -195,8 +237,8 @@ if __name__ == "__main__":
 
     max_epochs = 10000
     wandb.config = {
-        "learning_rate" : model.lr,
-        "epochs":  max_epochs,
+        "learning_rate": model.lr,
+        "epochs": max_epochs,
         "batch_size": batch_size,
         "alpha": model.alpha
     }
