Fix fid calculation bug

eval/fid_evaluator.py
@@ -10,12 +10,12 @@ def calculate_fid(validation_samples, generated_samples):
     gen = torch.cat(generated_samples, 1).squeeze(0).T
 
     mu1, sigma1 = torch.mean(val, dim=1), torch.cov(val)
-    mu2, sigma2 = torch.mean(gen, dim=1), torch.cov(val)
+    mu2, sigma2 = torch.mean(gen, dim=1), torch.cov(gen)
 
     mu1 = mu1.cpu().detach().numpy()
     mu2 = mu2.cpu().detach().numpy()
     sigma1 = sigma1.cpu().detach().numpy()
     sigma2 = sigma2.cpu().detach().numpy()
 
-    fid = fid_score.calculate_frechet_distance(mu1, sigma1, mu2, sigma2)
+    fid = fid_score.calculate_frechet_distance(mu2, sigma2, mu1, sigma1)
     return fid

models/simple_vae/simple_vae.py
@@ -19,7 +19,7 @@ import tqdm
 import numpy
 import wandb
 
-wandb.init(project="music-controllable-diffusion-with-fid-version-3", entity="saravanr")
+wandb.init(project="music-controllable-diffusion-with-fid-version-4", entity="saravanr")
 
 
 class ExtractLSTMOutput(nn.Module):
@@ -579,7 +579,7 @@ class SimpleVae(BaseModel):
 
 if __name__ == "__main__":
     print(f"Training simple VAE")
-    batch_size = 4096
+    batch_size = 2048
     train_mnist = False
 
     _alpha = 1